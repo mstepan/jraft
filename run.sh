b492d39d7642b7f0a9a4098c0d3baccd4f225094
Maksym Stepanenko <stepanenkomaksi@gmail.com>
2025-05-21 16:47:06 +0300

    Small cleanup

--- run.sh
+++ run.sh
@@ -4,6 +4,4 @@ JVM_THREADS="-Djdk.virtualThreadScheduler.parallelism=4 -Djdk.traceVirtualThread
 JVM_HEAP="-Xms1G -Xmx1G"
 JVM_JMX="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.local.only=true -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=127.0.0.1"
 
-JVM_OPTS="$JVM_THREADS $JVM_HEAP $JVM_JMX"
-
-java "$JVM_OPTS" --enable-preview -jar target/jraft-0.0.1-SNAPSHOT.jar
+java $JVM_THREADS $JVM_HEAP $JVM_JMX --enable-preview -jar target/jraft-0.0.1-SNAPSHOT.jar
